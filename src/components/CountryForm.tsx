4ec39211b848bf7813d00f8e3c22cf03cf1bc12d
SmileJayden <wkdwodud07@gmail.com>
2020-07-12 00:02:13 +0900

    make CountryForm using hookform yup

--- src/components/CountryForm.tsx
+++ src/components/CountryForm.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
+import { yupResolver } from '@hookform/resolvers';
+import * as yup from 'yup';
 
-interface FormData {
-  name: string;
-  alpha2Code: string;
-  callingCodes: string[];
-  capital: string;
-  region: RegionEnum;
-}
-
-enum RegionEnum {
-  Africa = 'Africa',
-  Asia = 'Asia',
-  Americas = 'Americas',
-  Europe = 'Europe',
-  Oceania = 'Oceania',
-}
+yup.setLocale({
+  number: {
+    min: 'Deve ser maior que ${min}',
+  },
+});
 
 const FormWrapper = styled.form`
   width: 300px;
@@ -55,8 +47,39 @@ const FormWrapper = styled.form`
   }
 `;
 
+interface FormData {
+  name: string;
+  alpha2Code: string;
+  callingCodes: number;
+  capital: string;
+  region: RegionEnum;
+}
+
+enum RegionEnum {
+  Africa = 'Africa',
+  Asia = 'Asia',
+  Americas = 'Americas',
+  Europe = 'Europe',
+  Oceania = 'Oceania',
+}
+
+const schema = yup.object().shape({
+  name: yup.string().required('국가 이름을 입력 하시오'),
+  alpha2Code: yup.string().required('국가 코드을 입력 하시오'),
+  callingCodes: yup
+    .number()
+    .typeError('숫자를 입력 하시오')
+    .positive('0보다 큰 수를 입력 하시오')
+    .integer('정수를 입력 하시오')
+    .required('국가 번호를 입력 하시오'), // never comes here... disappointed yup
+  capital: yup.string().required('수도를 입력 하시오'),
+  region: yup.string().required('대륙을 입력 하시오'),
+});
+
 const CountryForm = () => {
-  const { register, errors, handleSubmit } = useForm<FormData>();
+  const { register, errors, handleSubmit } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
   const onSubmit = (formData: FormData) => {
     console.log(formData);
   };
@@ -65,52 +88,32 @@ const CountryForm = () => {
     <FormWrapper onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="form-name">
         <span>국가 이름</span>
-        <input
-          id="form-name"
-          type="text"
-          name="name"
-          ref={register({ required: true })}
-        />
-        <p>{errors.name && '국가 이름을 입력 하시오'}</p>
+        <input id="form-name" type="text" name="name" ref={register()} />
+        <p>{errors.name?.message}</p>
       </label>
       <label htmlFor="form-code">
         <span>국가 코드</span>
-        <input
-          id="form-code"
-          type="text"
-          name="alpha2Code"
-          ref={register({ required: true })}
-        />
-        <p>{errors.alpha2Code && '국가 코드 입력 하시오'}</p>
+        <input id="form-code" type="text" name="alpha2Code" ref={register()} />
+        <p>{errors.alpha2Code?.message}</p>
       </label>
       <label htmlFor="form-calling-codes">
         <span>국가 번호</span>
         <input
           id="form-calling-codes"
-          type="number"
+          type="text"
           name="callingCodes"
-          ref={register({ required: true })}
+          ref={register()}
         />
-        <p>{errors.callingCodes && '국가 번호을 입력 하시오'}</p>
+        <p>{errors.callingCodes?.message}</p>
       </label>
       <label htmlFor="form-capital">
         <span>국가 수도 이름</span>
-        <input
-          id="form-capital"
-          type="text"
-          name="capital"
-          ref={register({ required: true })}
-        />
-        <p>{errors.capital && '국가의 수도 이름을 입력 하시오'}</p>
+        <input id="form-capital" type="text" name="capital" ref={register()} />
+        <p>{errors.capital?.message}</p>
       </label>
       <label htmlFor="form-name">
         <span>대륙</span>
-        <select
-          id="form-region"
-          name="region"
-          ref={register({ required: true })}
-          defaultValue=""
-        >
+        <select id="form-region" name="region" ref={register()} defaultValue="">
           <option value="" disabled hidden>
             Select your option
           </option>
@@ -120,7 +123,7 @@ const CountryForm = () => {
           <option value="Europe">Europe</option>
           <option value="Oceania">Oceania</option>
         </select>
-        <p>{errors.region && '대륙 선택 하시오'}</p>
+        <p>{errors.region?.message}</p>
       </label>
       <button type="submit">나라 추가 +</button>
     </FormWrapper>
